c9e48f648ffb69724029e515fc5a77ec103acc7e
Simeon Vladkov <80125788+snVladkov@users.noreply.github.com>
2024-10-01 14:50:02 +0300

    Update boilerplate.php

--- boilerplate.php
+++ boilerplate.php
@@ -15,7 +15,7 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark py-3">
             <div class="container-fluid">
-                <a class="navbar-brand fw-bold" href="/">Лаптопи без пари</a>
+                <a class="navbar-brand fw-bold" href="#">Лаптопи</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
