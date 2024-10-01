49bd03e4c60b566b0f610f7ea8218068aeb287e3
Simeon Vladkov <80125788+snVladkov@users.noreply.github.com>
2024-10-01 13:17:04 +0300

    Create boilerplate.php

---
+++ boilerplate.php
@@ -0,0 +1,68 @@
+<?php
+// boilerplate index
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Лаптопи</title>
+    <!-- Bootstrap 5.3 CSS CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark py-3">
+            <div class="container-fluid">
+                <a class="navbar-brand fw-bold" href="/">Лаптопи без пари</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Начало</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Продукти</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Контакти</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main class="container d-flex align-center py-4" style="min-height:80vh;">
+
+    </main>
+    <footer class="bg-dark text-center py-5 mt-auto">
+        <div class="container">
+            <span class="text-light">© 2024 All rights reserved</span>
+        </div>
+    </footer>
+</body>
+</html>
+
+<?php
+//страница home
+?>
+<div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+    <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+        <h1 class="display-4 fw-bold lh-1 text-body-emphasis">Най-добрите оферти за лаптопи!</h1>
+        <p class="lead">Изберете от широката ни продуктова гама лаптопи. Богато разнообразие от ценови класове и хардуерни конфигурации. Намерете своя нов лаптоп днес.</p>
+    </div>
+    <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
+        <img class="rounded-lg-3" src="https://cdn.thewirecutter.com/wp-content/media/2024/07/laptopstopicpage-2048px-3685-2x1-1.jpg" alt="" width="720">
+    </div>
+</div>
+
+<?php
+// страница продукти
+?>
+
+<?php
+// страница контакти
+?>
