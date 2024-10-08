c89171bf888cc73adc67eb1f27c4b7473d560f89
Simeon Vladkov <80125788+snVladkov@users.noreply.github.com>
2024-10-08 16:17:28 +0300

    Update sessions_cookies.php

--- sessions_cookies.php
+++ sessions_cookies.php
@@ -1,3 +1,35 @@
 <?php
-// страница login
+    // страница login
 ?>
+
+<form class="border rounded p-4 w-50 mx-auto">
+    <h3 class="text-center">Вход в системата</h3>
+    <div class="mb-3">
+        <label for="email" class="form-label">Имейл</label>
+        <input type="email" class="form-control" id="email">
+    </div>
+    <div class="mb-3">
+        <label for="password" class="form-label">Парола</label>
+        <input type="password" class="form-control" id="password">
+    </div>
+    <button type="submit" class="btn btn-primary mx-auto">Вход</button>
+</form>
+
+<!-- масив с потребителски данни за логин  -->
+$users = [
+    [
+        'email' => 'john@gmail.com',
+        'password' => '123456'
+        'name' => 'John Jones'
+    ],
+    [
+        'email' => 'ana@gmail.com',
+        'password' => 'qwerty'
+        'name' => 'Ana Smith'
+    ],
+    [
+        'email' => 'ivan@gmail.com',
+        'password' => 'asd123'
+        'name' => 'Ivan Ivanov'
+    ],
+];
