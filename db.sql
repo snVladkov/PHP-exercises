375f4aeac0d7e526077527afeea15850d4d0b9f1
Simeon Vladkov <80125788+snVladkov@users.noreply.github.com>
2024-10-22 15:41:05 +0300

    Create db.sql

---
+++ db.sql
@@ -0,0 +1,10 @@
+-- записи за продукти
+INSERT INTO products (image, title, price) VALUES
+('https://ardes.bg/uploads/original/dell-xps-13-plus-9320-393691.jpg', 'Dell XPS 13', '999.99'),
+('https://cdncloudcart.com/25880/products/images/28368/apple-macbook-air-m1-8gb-ddr4x-256gb-ssd-13-3-wqxga-gold-image_61a8cf36e50dc_600x600.jpeg?1638782461', 'Apple MacBook Air', '1199.99'),
+('https://img-cdn.tnwcdn.com/image?fit=1200%2C900&height=900&url=https%3A%2F%2Fcdn0.tnwcdn.com%2Fwp-content%2Fblogs.dir%2F1%2Ffiles%2F2021%2F08%2FHP-Spectre-x360-14-1-of-7.jpg&signature=0d2f21c8c102f0b5f8f935cee9d98ef6', 'HP Spectre x360', '1099.99'),
+('https://cdn.izotcomputers.com/8270-large_default/laptop-vtora-upotreba-lenovo-thinkpad-x1-carbon-gen-9.jpg', 'Lenovo ThinkPad X1 Carbon', '1299.99'),
+('https://laptop.bg/system/images/300015/normal/asus_zenbook_13_oled_ux325eaoledwb503r.jpg', 'Asus ZenBook 13', '899.99'),
+('https://gfx3.senetic.com/akeneo-catalog/7/1/b/9/71b955b8b0f63a78a44c1a6a845adbf09e281afc_1684023_LDH_00031_image1.jpg', 'Microsoft Surface Laptop 4', '999.99'),
+('https://ardes.bg/uploads/p/acer-swift-3-sf314-43-432825.jpg', 'Acer Swift 3', '699.99'),
+('https://ardes.bg/uploads/original/razer-blade-15-rz09-0421-428703.jpg', 'Razer Blade 15', '1599.99');
